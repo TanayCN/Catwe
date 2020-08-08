9e7b9945ffe008c78c985b2fe76f1d5db479d30f
tanay@sohu.com <258456xh123>
2020-08-08 14:55:19 +0800

    catwe orignversion

---
+++ src/main/java/cn/catwe/rassa/CatweApplication.java
@@ -0,0 +1,13 @@
+package cn.catwe.rassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/catwe/rassa/CatweApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.catwe.rassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
